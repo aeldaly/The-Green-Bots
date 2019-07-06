9bd731b3df7cd9c34b59eb52c35ea11007c167d2
Mehrdad <pazookime@gmail.com>
2019-07-06 15:49:41 -0400

    wifi exception

--- interfaces/web/server.py
+++ interfaces/web/server.py
@@ -212,7 +212,7 @@ network:
             SSIDs = [wifi.ssid for wifi in wifis]
             self.write(json.dumps(SSIDs))
         except Exception as ex:
-            self.write(json.dumps(ex))
+            self.write(json.dumps(str(ex)))
 
     def generate_wireless_yaml(self, data):
         return WifiHandler.WIRELESS_YAML_TEMPLATE % data
