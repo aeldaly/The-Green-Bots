9b6e30706e3c1d2d587aa42d5ffab74ecfda8d66
Mehrdad <pazookime@gmail.com>
2019-07-07 17:29:47 -0400

    Operate page added

---
+++ interfaces/web/assets/js/operate.js
@@ -0,0 +1,75 @@
+
+(function ($) {
+    $(document).ready(function () {
+        'use strict'
+
+        const cameraURL = "http://localhost:8000/api/operate/camera";
+        const controlUrl = "http://localhost:8000/api/operate/control";
+
+        var imageObj = new Image();
+        imageObj.onload = function () {
+            drawOnCanvas();
+            setTimeout(timedRefresh, 1);
+        }
+        // set src AFTER assigning load
+        $.get(cameraURL, function(data, status){
+            imageObj.src = "data:image/png;base64," + data;
+        });
+
+        function timedRefresh() {
+            $.get(cameraURL, function(data, status){
+                imageObj.src = "data:image/png;base64," + data;
+            });
+        }
+
+        function drawOnCanvas() {
+            var canvas = document.getElementById("canvas");
+            var ctx = canvas.getContext("2d");
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+        }
+
+        document.onkeydown = checkKey;
+
+        function checkKey(e) {
+            var controlKey = null;
+            e = e || window.event;
+            if (e.keyCode == '38') {
+                // up arrow
+                console.log('up arrow')
+                document.getElementById('control').value = 'Up';
+                controlKey = 'up';
+            }
+            else if (e.keyCode == '40') {
+                // down arrow
+                console.log('down arrow')
+                document.getElementById('control').value = 'Down';
+                controlKey = 'down';
+            }
+            else if (e.keyCode == '37') {
+            // left arrow
+                console.log('left arrow')
+                document.getElementById('control').value = 'Left';
+                controlKey = 'left';
+            }
+            else if (e.keyCode == '39') {
+            // right arrow
+                console.log('right arrow')
+                document.getElementById('control').value = 'Right';
+                controlKey = 'right';
+            }
+            if (controlKey != null){
+                $.ajax({
+                    url: controlUrl,
+                    type: 'post',
+                    dataType: 'json',
+                    data: JSON.stringify(controlKey),
+                    success: function(data) {
+                      console.log('Control key sent to backend...')
+                    }
+                });
+            }
+        }
+
+    });
+})($);
